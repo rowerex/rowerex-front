cb0762932800fae038e2e048791dbabf86d181c1
anastazjasta <aneta.dakalo@gmail.com>
2022-01-25 11:49:07 +0100

    fix: trash icon

--- src/components/MyTasks/Task.js
+++ src/components/MyTasks/Task.js
@@ -2,12 +2,16 @@ import React from "react";
 import classes from "./Task.module.scss";
 import Card from "../UI/Card";
 import { ReactComponent as ServiceIcon } from "../../assets/icons/task-service.svg";
+import { ReactComponent as TrashIcon } from "../../assets/icons/task-trash.svg";
+
 import Button from "../UI/Button/Button";
 
 const Task = (props) => {
   return (
     <Card className={classes.card}>
-      <ServiceIcon className={classes.icon} />
+      {props.taskType === "retire" && <TrashIcon className={classes.icon} />}
+      {props.taskType === "service" && <ServiceIcon className={classes.icon} />}
+
       <div className={classes.wrapper}>
         <p className={classes.partType}>{props.partType}</p>
         <h3>{props.partModel}</h3>

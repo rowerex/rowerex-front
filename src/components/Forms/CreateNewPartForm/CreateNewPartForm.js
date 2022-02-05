14c98874281f0ab95eda0b1b34c2f01bbf7fc8c3
anastazjasta <aneta.dakalo@gmail.com>
2022-02-05 15:09:01 +0100

    create new part form

--- src/components/Forms/CreateNewPartForm/CreateNewPartForm.js
+++ src/components/Forms/CreateNewPartForm/CreateNewPartForm.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Dropdown from "../../UI/Input/Dropdown";
 import Input from "../../UI/Input/Input";
 
 const CreateNewPartForm = () => {
   return (
     <form>
-      <Input name="Type" />
+      <Dropdown name="Type" />
+
       <Input name="Model" />
       <Input name="Name" />
       <Input name="Purchase date" type="date" />
---
+++ src/components/UI/Input/Dropdown.js
@@ -0,0 +1,73 @@
+import React from "react";
+import Select from "react-select";
+import classes from "./Input.module.scss";
+import "../../../common.scss";
+
+const aquaticCreatures = [
+  { label: "Shark", value: "Shark" },
+  { label: "Dolphin", value: "Dolphin" },
+  { label: "Whale", value: "Whale" },
+  { label: "Octopus", value: "Octopus" },
+  { label: "Crab", value: "Crab" },
+  { label: "Lobster", value: "Lobster" },
+];
+
+const customStyles = {
+  control: (provided, state) => ({
+    padding: "0 8px",
+    height: "42px",
+    minWidth: 240,
+    border: state.isFocused ? "2px solid #006b5c" : "1px solid #6f7976",
+    borderRadius: "4px",
+  }),
+
+  singleValue: () => ({
+    fontSize: 16,
+    letterSpacing: 0.5,
+    position: "absolute",
+    top: 10,
+  }),
+
+  input: () => ({
+    fontSize: 16,
+    letterSpacing: 0.5,
+    top: 20,
+    overflow: "hidden",
+  }),
+
+  menu: (state) => ({
+    position: "fixed",
+    zIndex: 100,
+    fontSize: 16,
+    letterSpacing: 0.5,
+    width: 240,
+  }),
+  indicatorsContainer: () => ({
+    position: "absolute",
+    top: 2,
+    right: 0,
+  }),
+  option: (provided, state) => ({
+    background: state.isFocused ? "#CAE1DC" : "#E6F1ED",
+    lineHeight: 2,
+    paddingLeft: 20,
+  }),
+  placeholder: () => ({
+    fontSize: 16,
+    letterSpacing: 0.5,
+    top: 20,
+  }),
+};
+
+const Dropdown = (props) => {
+  return (
+    <div className={classes.wrapper}>
+      <label className={classes.label} htmlFor={props.name}>
+        {props.name}
+      </label>
+      <Select styles={customStyles} options={aquaticCreatures} />
+    </div>
+  );
+};
+
+export default Dropdown;

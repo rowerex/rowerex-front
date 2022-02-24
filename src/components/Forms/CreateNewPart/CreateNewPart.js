2c6b3bc0bfa68331aba17d0aafb98459381f6677
anastazjasta <aneta.dakalo@gmail.com>
2022-02-24 13:15:06 +0100

    sending POST call with new part to API

--- src/components/Forms/CreateNewPart/CreateNewPart.js
+++ src/components/Forms/CreateNewPart/CreateNewPart.js
@@ -3,97 +3,100 @@ import Dropdown from "../../UI/Input/Dropdown";
 import Input from "../../UI/Input/Input";
 import Button from "../../UI/Button/Button";
 import useTypes from "../../../services/useTypes";
+import classes from "./Form.module.scss";
 
 const CreateNewPart = (props) => {
 
-  const [selectedType, setSelectedType] = useState("");
+    const [selectedType, setSelectedType] = useState("");
+    const [selectedModel, setSelectedModel] = useState("");
 
-  const modelRef = useRef(null);
-  const nameRef = useRef(null);
-  const typeRef = useRef(null);
-  const [models, setModels] = useState([]);
-  const productionDateRef = useRef(null);
-  const [types, typesError, typesLoading] = useTypes();
-  const typeOptions = types.map((type) => {
-    const option = {};
-    option.label = type.name;
-    option.value = type.id;
-    return option;
-  })
-  const modelOptions = models.map((model) => {
-    const option = {};
-    option.label = model.name;
-    option.value = model.id;
-    return option;
-  })
+    const nameRef = useRef(null);
+    const [models, setModels] = useState([]);
+    const productionDateRef = useRef(null);
+    const [types, typesError, typesLoading] = useTypes();
+    const typeOptions = types.map((type) => {
+        const option = {};
+        option.label = type.name;
+        option.value = type.id;
+        return option;
+    })
+    const modelOptions = models.map((model) => {
+        const option = {};
+        option.label = model.name;
+        option.value = model.id;
+        return option;
+    })
 
-  const getToken = () => {
-    const tokenString = sessionStorage.getItem('token');
-    const userToken = JSON.parse(tokenString);
-    return userToken?.token
-  };
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-
-    const newPart = {
-      name: nameRef.current.value,
-      modelId: modelRef.current.value,
-      productionDate: productionDateRef.current.value,
+    const getToken = () => {
+        const tokenString = sessionStorage.getItem('token');
+        const userToken = JSON.parse(tokenString);
+        return userToken?.token
     };
 
-    console.log(newPart);
-    addPart(newPart);
-  };
+    const submitHandler = (event) => {
+        event.preventDefault();
 
-  useEffect(()=> {
-    async function getModels(selectedType) {
-      const token = getToken();
-      console.log(token);
-      const response = await fetch("http://localhost:8080/api/models?type="+ selectedType, {
-        method: "GET",
-        headers: {
-          'X-AUTH-TOKEN': token,
-          "Content-Type": "application/json",
-        },
+        const newPart = {
+            name: nameRef.current.value,
+            modelId: selectedModel,
+            productionDate: productionDateRef.current.value,
+        };
 
-      });
-      const data = await response.json();
-      console.log(data);
-      setModels(data);
+        console.log(newPart);
+        addPart(newPart);
+    };
+
+    useEffect(() => {
+        async function getModels(selectedType) {
+            const token = getToken();
+            console.log(token);
+            const response = await fetch("http://localhost:8080/api/models?type=" + selectedType, {
+                method: "GET",
+                headers: {
+                    'X-AUTH-TOKEN': token,
+                    "Content-Type": "application/json",
+                },
+
+            });
+            const data = await response.json();
+            console.log(data);
+            setModels(data);
+        }
+
+        getModels(selectedType)
+
+    }, [selectedType])
+
+    async function addPart(part) {
+        const token = getToken();
+        console.log(token);
+        const response = await fetch("http://localhost:8080/api/parts", {
+            method: "POST",
+            body: JSON.stringify(part),
+            headers: {
+                'X-AUTH-TOKEN': token,
+                "Content-Type": "application/json",
+            },
+
+        });
+        const data = await response.json();
+        console.log(data);
     }
-    getModels(selectedType)
 
+    return (
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Dropdown value={selectedType} name="Type" options={typeOptions}
+                      onChange={event => setSelectedType(event.value)}/>
+            <Dropdown value={selectedModel} isRequired={true} options={modelOptions} name="Model"
+                      onChange={event => setSelectedModel(event.value)}/>
+            <Input isRequired={true} name="Name" ref={nameRef}/>
+            <Input isRequired={true} name="Purchase date" type="date" ref={productionDateRef}/>
 
-  },[selectedType])
-  async function addPart(part) {
-    const token = getToken();
-    console.log(token);
-    const response = await fetch("http://localhost:8080/api/parts", {
-      method: "POST",
-      body: JSON.stringify(part),
-      headers: {
-        'X-AUTH-TOKEN': token,
-        "Content-Type": "application/json",
-      },
-
-    });
-    const data = await response.json();
-    console.log(data);
-  }
-
-  return (
-    <form onSubmit={submitHandler}>
-      <Dropdown value={selectedType} name="Type" options={typeOptions} ref={typeRef} onChange={event => setSelectedType(event.value)}/>
-      <Dropdown isRequired={true} options={modelOptions} name="Model" ref={modelRef}/>
-      <Input isRequired={true} name="Name" ref={nameRef}/>
-      <Input isRequired={true} name="Purchase date" type="date" ref={productionDateRef}/>
-
-      <Button size="big" type="submit" onClick={props.onConfirm}>
-        Create New Part
-      </Button>
-    </form>
-  );
+            <Button classes={classes.button} size="big" type="submit" onClick={props.onConfirm}>
+                Create New Part
+            </Button>
+        </form>
+    );
 };
 
 export default CreateNewPart;

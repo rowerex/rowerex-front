e08d9ccc1b71c08cbcc50d8178ccf95568ad1b19
anastazjasta <aneta.dakalo@gmail.com>
2022-04-21 21:51:23 +0200

    refactor: send login request using useHttp hook

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,58 +1,71 @@
-import React, { useState } from "react";
-import { postLogin } from "../../services/requests";
+import React, {useState} from "react";
 import Button from "../UI/Button/Button";
 import Input from "../UI/Input/Input";
 import classes from "./Login.module.scss";
+import useHttp from "../../hooks/useHttp";
 
-async function loginUser(credentials) {
-  return fetch(...postLogin(credentials)).then((data) => data.json());
+const Login = ({setToken}) => {
+    const {isLoading, error, sendRequest: sendLoginRequest} = useHttp();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const loginUserHandler = (token) => {
+        setToken(token);
+    }
+
+    const handleSubmit = async (e) => {
+        console.log(username);
+        console.log(password);
+        e.preventDefault();
+        sendLoginRequest({
+            path: "/login", method: "POST", body: {
+                username,
+                password,
+            },
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }, loginUserHandler);
+    };
+
+    const usernameChangeHandler = (e) => {
+        setUsername(e.target.value);
+        console.log(username);
+    };
+    const passwordChangeHandler = (e) => {
+        setPassword(e.target.value);
+        console.log(password);
+    };
+
+    if (error) {
+        return <p>Error logging in: {error}</p>;
+    }
+    if (isLoading) {
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <>
+            <h1>Please Log In</h1>
+            <form onSubmit={handleSubmit}>
+                <Input
+                    name="username"
+                    value={username.value}
+                    onChange={usernameChangeHandler}
+                />
+                <Input
+                    name="password"
+                    type="password"
+                    value={password.value}
+                    onChange={passwordChangeHandler}
+                />
+                <div>
+                    <Button type="submit" size="big">
+                        Login
+                    </Button>
+                </div>
+            </form>
+        </>
+    );
 }
-
-export default function Login({ setToken }) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = async (e) => {
-    console.log(username);
-    console.log(password);
-    e.preventDefault();
-    const token = await loginUser({
-      username,
-      password,
-    });
-    setToken(token);
-  };
-
-  const usernameChangeHandler = (e) => {
-    setUsername(e.target.value);
-    console.log(username);
-  };
-  const passwordChangeHandler = (e) => {
-    setPassword(e.target.value);
-    console.log(password);
-  };
-
-  return (
-    <>
-      <h1>Please Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <Input
-          name="username"
-          value={username.value}
-          onChange={usernameChangeHandler}
-        />
-        <Input
-          name="password"
-          type="password"
-          value={password.value}
-          onChange={passwordChangeHandler}
-        />
-        <div>
-          <Button type="submit" size="big">
-            Login
-          </Button>
-        </div>
-      </form>
-    </>
-  );
-}
+export default Login;

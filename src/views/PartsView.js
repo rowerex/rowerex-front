98a73486998534590f3f6eb7807406e01d0672e6
anastazjasta <aneta.dakalo@gmail.com>
2022-05-23 13:05:34 +0200

    fix: last list element is full visible

--- src/views/PartsView.js
+++ src/views/PartsView.js
@@ -5,6 +5,7 @@ import Parts from "../components/Parts/Parts";
 import Modal from "../components/UI/Modal/Modal";
 import Button from "../components/UI/Button/Button";
 import CreateNewPart from "../components/Forms/CreateNewPart";
+import classes from "./view.module.scss";
 
 const PartsView = () => {
     const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -19,18 +20,20 @@ const PartsView = () => {
 
     return (
         <>
-            <Header image={Image} alt="cat looking at the bike part">
-                My Parts
-            </Header>
-            {modalIsOpen === true && (
-                <Modal
-                    title="Create new Part"
-                    onClose={closeModalHandler}
-                >
-                    <CreateNewPart onSuccess={closeModalHandler}/>
-                </Modal>
-            )}
-            <Parts/>
+            <div className={classes.viewContainer}>
+                <Header image={Image} alt="cat looking at the bike part">
+                    My Parts
+                </Header>
+                {modalIsOpen === true && (
+                    <Modal
+                        title="Create new Part"
+                        onClose={closeModalHandler}
+                    >
+                        <CreateNewPart onSuccess={closeModalHandler}/>
+                    </Modal>
+                )}
+                <Parts/>
+            </div>
             <Button size="fab" variant="add" onClick={openModalHandler}>
                 Create part
             </Button>

c68fc53bfaa64e65f2e605a87c619ed62db02550
anastazjasta <aneta.dakalo@gmail.com>
2022-05-25 15:01:14 +0200

    fix: background color & buttons height

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -6,79 +6,77 @@ import {useNavigate} from "react-router-dom";
 import classes from "./LoginView.module.scss";
 
 const LoginView = ({setToken}) => {
-    const {isLoading, error, sendRequest: sendLoginRequest} = useHttp();
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
+  const {isLoading, error, sendRequest: sendLoginRequest} = useHttp();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
 
-    const loginUserHandler = (token) => {
-        setToken(token);
-        navigate("/");
-    }
+  const loginUserHandler = (token) => {
+    setToken(token);
+    navigate("/");
+  }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        sendLoginRequest({
-            path: "/login", method: "POST", body: {
-                username,
-                password,
-            },
-            headers: {
-                "Content-Type": "application/json",
-            },
-        }, loginUserHandler);
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    sendLoginRequest({
+      path: "/login", method: "POST", body: {
+        username,
+        password,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }, loginUserHandler);
+  };
 
-    const handleRegisterClick = () => {
-            navigate("/register");
-    }
+  const handleRegisterClick = () => {
+    navigate("/register");
+  }
 
-    const usernameChangeHandler = (e) => {
-        setUsername(e.target.value);
-    };
-    const passwordChangeHandler = (e) => {
-        setPassword(e.target.value);
-    };
+  const usernameChangeHandler = (e) => {
+    setUsername(e.target.value);
+  };
+  const passwordChangeHandler = (e) => {
+    setPassword(e.target.value);
+  };
 
-    if (error) {
-        return <p>Error logging in: {error}</p>;
-    }
-    if (isLoading) {
-        return <p>Loading...</p>;
-    }
+  if (error) {
+    return <p>Error logging in: {error}</p>;
+  }
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
-    return (
-        <div className={classes.formContainer}>
-            <h1>Log In</h1>
-            <p className={classes.content}>Never lose access to your gear<br/>and bike care reminders
-            </p>
-            <form onSubmit={handleSubmit}>
-                <Input
-                    isRequired={true}
-                    autocomplete="username"
-                    name="username"
-                    value={username.value}
-                    onChange={usernameChangeHandler}
-                />
-                <Input
-                    isRequired={true}
-                    name="password"
-                    type="password"
-                    autocomplete="current-password"
-                    value={password.value}
-                    onChange={passwordChangeHandler}
-                />
-                <div className={classes.buttonContainer}>
-                    <Button type="submit" size="big">
-                        Login
-                    </Button>
-                    <Button  size="big" priority="secondary" onClick={handleRegisterClick}>
-                        Register
-                    </Button>
-                </div>
-            </form>
-        </div>
-    );
+  return (
+    <div className={classes.formContainer}>
+      <h1>Log In</h1>
+      <p className={classes.content}>Never lose access to your gear<br/>and bike care reminders
+      </p>
+      <form onSubmit={handleSubmit}>
+        <Input
+          isRequired={true}
+          autocomplete="username"
+          name="username"
+          value={username.value}
+          onChange={usernameChangeHandler}
+        />
+        <Input
+          isRequired={true}
+          name="password"
+          type="password"
+          autocomplete="current-password"
+          value={password.value}
+          onChange={passwordChangeHandler}
+        />
+        <Button type="submit" size="big">
+          Login
+        </Button>
+        <Button size="big" priority="secondary" onClick={handleRegisterClick}>
+          Register
+        </Button>
+      </form>
+    </div>
+  );
 }
 export default LoginView;

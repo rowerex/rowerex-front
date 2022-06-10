2378f2b35da8e6c63465c0a81075f7f5152fac3b
anastazjasta <aneta.dakalo@gmail.com>
2022-06-10 18:09:19 +0200

    feat: import rides from strava
    Took 1 minute

--- src/views/UserView.js
+++ src/views/UserView.js
@@ -3,8 +3,15 @@ import Button from "../components/UI/Buttons/Button";
 import {useNavigate} from "react-router-dom";
 import TokenContext from "../store/TokenContext";
 import classes from './ListView.module.scss';
+import useHttp from "../hooks/useHttp";
+import PartsContext from "../store/PartsContext";
+import BikesContext from "../store/BikesContext";
 
 const UserVIew = () => {
+  const {isLoading, error, sendRequest: getRides} = useHttp();
+  const {partsDispatcher} = useContext(PartsContext);
+  const {bikesDispatcher} = useContext(BikesContext);
+
   const {setToken} = useContext(TokenContext)
   const navigate = useNavigate();
 
@@ -13,12 +20,25 @@ const UserVIew = () => {
     navigate("/login");
   }
 
+  const importHandler = () => {
+    partsDispatcher({type: "INVALIDATE_PARTS"});
+    bikesDispatcher({type: "INVALIDATE_BIKES"});
+  }
+
+  const handleImportClick = () => {
+    getRides({
+      method: "GET",
+      path: "/strava/rides/update",
+    }, importHandler);
+  }
+
   return (
     <div className={classes.viewContainer}>
       <h2>
         User
       </h2>
       <Button size="big" onClick={handleLogoutClick}>Logout</Button>
+      <Button size="big" onClick={handleImportClick}>Import rides from strava</Button>
     </div>
   );
 };

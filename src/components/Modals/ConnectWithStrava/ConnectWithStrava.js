0a5002d5d68f0e8415b0225299602b59ef58431d
anastazjasta <aneta.dakalo@gmail.com>
2022-06-22 17:25:47 +0200

    fix: don't show 'connect to strava modal' if strava is already connected
    Took 57 minutes

--- src/components/Modals/ConnectWithStrava/ConnectWithStrava.js
+++ src/components/Modals/ConnectWithStrava/ConnectWithStrava.js
@@ -4,28 +4,30 @@ import useToken from "../../../services/useToken";
 import useHttp from "../../../hooks/useHttp";
 
 const ConnectWithStrava = () => {
-    const {isLoading, error, sendRequest} = useHttp();
+  const {isLoading, error, sendRequest: getStravaURL} = useHttp();
     const [authorizationUrl, setAuthorizationUrl] = useState("");
     const [buttonClicked, setButtonClicked] = useState(false);
     const {token} = useToken();
 
-    console.log(authorizationUrl)
+  console.log(authorizationUrl)
 
 
-    useEffect(() => {
-        const enableStravaConnection = (data) => {
-            setAuthorizationUrl(data.authorizationUrl);
-        };
-        sendRequest({
-            method: "GET",
-            path: "/strava/authorization-url",
-            headers: {
-                'X-AUTH-TOKEN': token,
-                "Content-Type": "application/json",
-            },
-        }, enableStravaConnection);
+  useEffect(() => {
+    const enableStravaConnection = (data) => {
+      setAuthorizationUrl(data.authorizationUrl);
+    };
+    getStravaURL({
+      method: "GET",
+      path: "/strava/authorization-url",
+      headers: {
+        'X-AUTH-TOKEN': token,
+        "Content-Type": "application/json",
+      },
+    }, enableStravaConnection);
 
-    }, [sendRequest])
+  }, [getStravaURL]);
+
+
 
     const handleButtonClick = () => {
         window.open(authorizationUrl, "_self");
--- src/views/ConnectedView.js
+++ src/views/ConnectedView.js
@@ -1,13 +1,18 @@
-import React, {useEffect, useState} from "react";
-import {NavLink} from "react-router-dom";
+import React, {useContext, useEffect, useState} from "react";
+import { useNavigate } from "react-router-dom";
 import useHttp from "../hooks/useHttp";
 import useQuery from "../hooks/useQuery";
 import Button from "../components/UI/Buttons/Button";
+import classes from "./ListView.module.scss";
+import Modal from "../components/UI/Modal/Modal";
+import UserContext from "../store/UserContext";
 
 const ConnectedView = () => {
-    const {isLoading, error, sendRequest} = useHttp();
+    const {sendRequest: sendStravaCode} = useHttp();
     const [connected, setConnected] = useState(false);
+    const {userDispatcher} = useContext(UserContext);
 
+  let navigate = useNavigate();
     let code = null;
     let queryParams = useQuery();
     code = queryParams.get('code');
@@ -16,8 +21,9 @@ const ConnectedView = () => {
         const submitStravaCode = () => {
             console.log("set connected")
             setConnected(true);
+            userDispatcher({type: "INVALIDATE_USER"});
         };
-        sendRequest({
+        sendStravaCode({
             method: "POST",
             path: "/strava/authorize",
             body: {
@@ -25,17 +31,24 @@ const ConnectedView = () => {
             }
         }, submitStravaCode);
 
-    }, [sendRequest, code])
+    }, [sendStravaCode, code])
+
+    const closeModalHandler = () => {
+        navigate("/");
+    }
 
     return (
-        <>
+      <div className={classes.viewContainer}>
             {!connected ? <p>Loading...</p> : <>
-            <p>Successfully connected with Strava!</p>
-            <NavLink to="/">
-                <Button size="big">Ok</Button>
-            </NavLink>
+                <Modal
+                  title="Strava connection"
+                onClose={closeModalHandler}
+                >
+                    <p>Successfully connected with Strava!</p>
+                        <Button size="big" onClick={closeModalHandler}>Ok</Button>
+                </Modal>
             </> }
-        </>
+        </div>
     );
 }
 
